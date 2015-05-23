4a1e500c151485ca085b4e8219cf91ed40bf43fe
Thomas de Candia <thomasd@ai.sri.com>
2015-05-22 18:29:00 -0700

    Add bundle concept to image_marker_detector.

--- ar_track_alvar/nodes/image_marker_detector.cpp
+++ ar_track_alvar/nodes/image_marker_detector.cpp
@@ -42,11 +42,14 @@
 #include "ar_track_alvar/MarkerDetector.h"
 #include "ar_track_alvar/Shared.h"
 
+#include <ar_track_alvar/filter/medianFilter.h>
 #include <ar_track_alvar/ParamsConfig.h>
 
 #include <ar_track_alvar_msgs/AlvarMarkers.h>
 
 #include <tf/transform_listener.h>
+
+#include <eigen_conversions/eigen_msg.h>
 
 #include <sensor_msgs/image_encodings.h>
 
@@ -69,7 +72,7 @@ public:
    * Constructor
    * @param parameters detector parameters
    */
-  MarkerDetectorNode();
+  MarkerDetectorNode(std::vector<Bundle>& bundles);
 
   /**
    * Destructor
@@ -86,10 +89,16 @@ private:
   typedef boost::shared_ptr<tf::TransformListener> TransformListenerPtr;
   typedef boost::shared_ptr<tf::TransformBroadcaster> TransformBroadcasterPtr;
   typedef boost::shared_ptr<image_transport::ImageTransport> ImageTransportPtr;
+  typedef boost::shared_ptr<ar_track_alvar::MedianFilter> MedianFilterPtr;
 
   void imageCallback(const sensor_msgs::ImageConstPtr& p_image_msg);
+  int updateBundle(Bundle& bundle);
 
   MarkerDetector<MarkerData> marker_detector_;
+  std::vector<Bundle> bundles_;
+  IdxToIdxMap marker_to_local_idxs_;
+  std::vector<tf::Transform> marker_poses_;
+  std::vector<bool> detected_markers_;
   ros::NodeHandle node_handle_;
   ImageTransportPtr p_image_transport_;
   CameraPtr p_camera_;
@@ -104,14 +113,19 @@ private:
   std::string reference_frame_;
   cv_bridge::CvImagePtr p_cv_bridge_image_;
   ar_track_alvar_msgs::AlvarMarkers alvar_markers_msg_;
+  std::vector<MedianFilterPtr> median_filters_;
+  int median_filter_size_;
 };
 
-MarkerDetectorNode::MarkerDetectorNode()
-  : node_handle_("~")
+MarkerDetectorNode::MarkerDetectorNode(std::vector<Bundle>& bundles)
+  : bundles_(bundles)
+  , node_handle_("~")
   , p_image_transport_(new image_transport::ImageTransport(node_handle_))
   , p_tf_listener_(new tf::TransformListener(node_handle_))
   , p_tf_broadcaster_(new tf::TransformBroadcaster())
-{}
+{
+  median_filters_.resize(bundles.size());
+}
 
 MarkerDetectorNode::~MarkerDetectorNode()
 {}
@@ -128,12 +142,35 @@ void MarkerDetectorNode::run()
   node_handle_.param("camera_info_topic", camera_info_topic, std::string("/camera/rgb/camera_info"));
   node_handle_.param("reference_frame", reference_frame_, std::string("camera_link"));
   node_handle_.param("process_rate", process_rate, 50.0);
+  node_handle_.param("median_filter_size", median_filter_size_, 0);
 
   if (marker_size_ <= 0.0)
   {
     ROS_ERROR_STREAM("Invalid marker size: " << marker_size_ << " should be > 0 cm");
     return;
   }
+
+  marker_to_local_idxs_.clear();
+  marker_poses_.clear();
+  for (size_t b = 0; b < bundles_.size(); ++b)
+  {
+    const Bundle& bundle = bundles_[b];
+    marker_to_local_idxs_[bundle.master_idx] = marker_poses_.size();
+    marker_poses_.push_back(tf::Transform());
+
+    for (size_t m = 0; m < bundle.marker_idxs.size(); ++m)
+    {
+      marker_to_local_idxs_[bundle.marker_idxs[m]] = marker_poses_.size();
+      marker_poses_.push_back(tf::Transform());
+    }
+
+    if (median_filter_size_ > 0)
+    {
+      median_filters_[b] = MedianFilterPtr(new ar_track_alvar::MedianFilter(median_filter_size_));
+    }
+  }
+
+  detected_markers_.assign(marker_poses_.size(), false);
 
   marker_detector_.SetMarkerSize(marker_size_);
 
@@ -181,38 +218,70 @@ void MarkerDetectorNode::imageCallback(const sensor_msgs::ImageConstPtr& p_image
 
       marker_detector_.Detect(&ipl_image, p_camera_.get(), true, false, max_new_marker_error_, max_track_error_, CVSEQ, true);
 
-      alvar_markers_msg_.markers.clear();
-
+      detected_markers_.assign(marker_poses_.size(), false);
       for (size_t m = 0; m < marker_detector_.markers->size(); ++m)
       {
-        int id = (*(marker_detector_.markers))[m].GetId();
-        // bool is_valid = (*(marker_detector_.markers))[m].valid;
-        bool is_valid = true;
+        int idx = (*(marker_detector_.markers))[m].GetId();
 
-        if ( (id >= 0) &&
-             is_valid )
+        if (idx >= 0)
         {
           tf::Transform pose;
           bool is_pose_valid = toRosPose((*(marker_detector_.markers))[m].pose, pose);
           if (is_pose_valid)
           {
-            std::string marker_frame;
-            getMarkerFrame(id, marker_frame);
-
-            tf::StampedTransform marker_wrt_camera(pose, p_image_msg->header.stamp, p_image_msg->header.frame_id, marker_frame);
-            p_tf_broadcaster_->sendTransform(marker_wrt_camera);
-
-            tf::Transform marker_wrt_reference = cam_wrt_reference * pose;
-
-            ar_track_alvar_msgs::AlvarMarker marker_msg;
-            makeMarkerMsg(id, reference_frame_, p_image_msg->header.stamp, marker_wrt_reference, 1, marker_msg);
-            alvar_markers_msg_.markers.push_back(marker_msg);
+            IdxToIdxMap::iterator iterator = marker_to_local_idxs_.find(idx);
+            if (iterator != marker_to_local_idxs_.end())
+            {
+              int local_idx = iterator->second;
+              marker_poses_[local_idx] = pose;
+              detected_markers_[local_idx] = true;
+            }
 
             visualization_msgs::Marker viz_marker_msg;
-            makeVizMarkerMsg(VISIBLE_MARKER, id, p_image_msg->header, pose, marker_size_, viz_marker_msg);
+            makeVizMarkerMsg(VISIBLE_MARKER, idx, p_image_msg->header, pose, marker_size_, viz_marker_msg);
 
             viz_marker_publisher_.publish(viz_marker_msg);
           }
+        }
+      }
+
+      alvar_markers_msg_.markers.clear();
+      for (size_t b = 0; b < bundles_.size(); ++b)
+      {
+        int marker_seen = updateBundle(bundles_[b]);
+
+        if (marker_seen > 0)
+        {
+          if (median_filter_size_ > 0)
+          {
+            Pose alvar_pose;
+            toAlvarPose(bundles_[b].pose, alvar_pose);
+
+            median_filters_[b]->addPose(alvar_pose);
+            median_filters_[b]->getMedian(alvar_pose);
+
+            toRosPose(alvar_pose, bundles_[b].pose);
+          }
+
+          int master_idx = bundles_[b].master_idx;
+          tf::Transform bundle_pose =  bundles_[b].pose;
+
+          std::string marker_frame;
+          getMarkerFrame(master_idx, marker_frame);
+
+          tf::StampedTransform marker_wrt_camera(bundle_pose, p_image_msg->header.stamp, p_image_msg->header.frame_id, marker_frame);
+          p_tf_broadcaster_->sendTransform(marker_wrt_camera);
+
+          tf::Transform marker_wrt_reference = cam_wrt_reference * bundle_pose;
+
+          ar_track_alvar_msgs::AlvarMarker marker_msg;
+          makeMarkerMsg(master_idx, reference_frame_, p_image_msg->header.stamp, marker_wrt_reference, marker_seen, marker_msg);
+          alvar_markers_msg_.markers.push_back(marker_msg);
+
+          visualization_msgs::Marker viz_marker_msg;
+          makeVizMarkerMsg(MAIN_MARKER, master_idx, p_image_msg->header, bundle_pose, marker_size_, viz_marker_msg);
+
+          viz_marker_publisher_.publish(viz_marker_msg);
         }
       }
 
@@ -225,13 +294,89 @@ void MarkerDetectorNode::imageCallback(const sensor_msgs::ImageConstPtr& p_image
   }
 }
 
+int MarkerDetectorNode::updateBundle(Bundle& bundle)
+{
+  int seen_marker = 0;
+
+  geometry_msgs::Pose marker_wrt_camera;
+
+  Eigen::Vector3d bundle_position(0.0, 0.0, 0.0);
+  Eigen::Vector4d bundle_orientation(0.0, 0.0, 0.0, 0.0);
+
+  int local_idx = marker_to_local_idxs_[bundle.master_idx];
+  if (detected_markers_[local_idx])
+  {
+    tf::poseTFToMsg(marker_poses_[local_idx], marker_wrt_camera);
+
+    Eigen::Vector3d position;
+    tf::pointMsgToEigen(marker_wrt_camera.position, position);
+
+    Eigen::Quaterniond orientation;
+    tf::quaternionMsgToEigen(marker_wrt_camera.orientation, orientation);
+    orientation.normalize();
+
+    bundle_position += position;
+    bundle_orientation += Eigen::Vector4d(orientation.w(), orientation.x(), orientation.y(), orientation.z());
+
+    ++seen_marker;
+  }
+
+  for (size_t m = 0; m < bundle.marker_idxs.size(); ++m)
+  {
+    int local_idx = marker_to_local_idxs_[bundle.marker_idxs[m]];
+    if (detected_markers_[local_idx])
+    {
+      tf::poseTFToMsg(marker_poses_[local_idx] * bundle.marker_pose_constraints[m], marker_wrt_camera);
+
+      Eigen::Vector3d position;
+      tf::pointMsgToEigen(marker_wrt_camera.position, position);
+
+      Eigen::Quaterniond orientation;
+      tf::quaternionMsgToEigen(marker_wrt_camera.orientation, orientation);
+      orientation.normalize();
+
+      bundle_position += position;
+      bundle_orientation += Eigen::Vector4d(orientation.w(), orientation.x(), orientation.y(), orientation.z());
+
+      ++seen_marker;
+    }
+  }
+
+  if (seen_marker > 0)
+  {
+    double n = static_cast<double>(seen_marker);
+    bundle_position = bundle_position / n;
+    bundle_orientation = bundle_orientation / n;
+
+    Eigen::Quaterniond orientation(bundle_orientation(0), bundle_orientation(1), bundle_orientation(2), bundle_orientation(3));
+    orientation.normalize();
+
+    geometry_msgs::Pose bundle_pose;
+    tf::pointEigenToMsg(bundle_position, bundle_pose.position);
+    tf::quaternionEigenToMsg(orientation, bundle_pose.orientation);
+
+    tf::poseMsgToTF(bundle_pose, bundle.pose);
+  }
+
+  return seen_marker;
+}
+
 int main(int argc, char *argv[])
 {
   ros::init(argc, argv, "image_marker_detector");
 
+  std::vector<std::string> bundle_config_list;
+  for (int a = 1; a < argc; ++a)
+  {
+    bundle_config_list.push_back(std::string(argv[a]));
+  }
+
+  std::vector<Bundle> bundles;
+  getBundleConfigurations(bundle_config_list, bundles);
+
   try
   {
-    MarkerDetectorNode marker_detector_node;
+    MarkerDetectorNode marker_detector_node(bundles);
     marker_detector_node.run();
   }
   catch (std::exception& exception)
--- ar_track_alvar/nodes/marker_detector_helper.h
+++ ar_track_alvar/nodes/marker_detector_helper.h
@@ -6,12 +6,106 @@
 #include <tf/tf.h>
 
 #include <sstream>
+#include <map>
+
+#ifdef AR_TRACK_ALVAR_HAVE_YAMLCPP_05
+#include <yaml-cpp/yaml.h>
+#else
+#include <yaml-cpp/node.h>
+#include <yaml-cpp/parser.h>
+#endif
 
 #include <math.h>
 
 #define MAIN_MARKER 1
 #define VISIBLE_MARKER 2
 #define GHOST_MARKER 3
+
+typedef std::map<int, size_t> IdxToIdxMap;
+
+struct Bundle
+{
+  int master_idx;
+  tf::Transform pose;
+  std::vector<int> marker_idxs;
+  std::vector<tf::Transform> marker_pose_constraints;
+};
+
+typedef std::vector<Bundle> BundleList;
+
+void getBundleConfiguration(const std::string& configuration_file, Bundle& bundle)
+{
+  bundle.marker_idxs.clear();
+  bundle.marker_pose_constraints.clear();
+
+  YAML::Node configuration_node;
+#ifdef AR_TRACK_ALVAR_HAVE_YAMLCPP_05
+  configuration_node = YAML::LoadFile(configuration_file.c_str());
+#else
+  std::ifstream input_file_stream(configuration_file.c_str());
+  YAML::Parser parser(input_file_stream);
+  parser.GetNextDocument(configuration_node);
+#endif
+
+  std::cout << "configuration_node.Type(): " << configuration_node.Type() << std::endl;
+  assert(configuration_node.Type() == YAML::NodeType::Map);
+  const YAML::Node& master_id_node = configuration_node["master_id"];
+
+#ifdef AR_TRACK_ALVAR_HAVE_YAMLCPP_05
+  bundle.master_idx = master_id_node.as<int>();
+#else
+   master_id_node >> bundle.master_idx;
+#endif
+
+  const YAML::Node& bundle_list_node = configuration_node["bundle"];
+
+  for (size_t i = 0; i < bundle_list_node.size(); ++i)
+  {
+    const YAML::Node& marker_node = bundle_list_node[i];
+    const YAML::Node& pose_node = marker_node["pose"];
+    const YAML::Node& position_node = pose_node["position"];
+    const YAML::Node& orientation_node = pose_node["orientation"];
+
+    int id;
+    double x, y, z, roll, pitch, yaw;
+#ifdef AR_TRACK_ALVAR_HAVE_YAMLCPP_05
+    id = marker_node["id"].as<int>();
+    x = position_node["x"].as<double>();
+    y = position_node["y"].as<double>();
+    z = position_node["z"].as<double>();
+    roll = orientation_node["r"].as<double>();
+    pitch = orientation_node["p"].as<double>();
+    yaw = orientation_node["y"].as<double>();
+#else
+    marker_node["id"] >> id;
+    position_node["x"] >> x;
+    position_node["y"] >> y;
+    position_node["z"] >> z;
+    orientation_node["r"] >> roll;
+    orientation_node["p"] >> pitch;
+    orientation_node["y"] >> yaw;
+#endif
+
+    tf::Vector3 position(x, y, z);
+    tf::Quaternion orientation = tf::createQuaternionFromRPY(roll, pitch, yaw);
+
+    bundle.marker_idxs.push_back(id);
+    bundle.marker_pose_constraints.push_back(tf::Transform(orientation, position).inverse());
+  }
+}
+
+void getBundleConfigurations(const std::vector<std::string>& configuration_file_list, BundleList& bundles)
+{
+  bundles.clear();
+
+  for (size_t i = 0; i < configuration_file_list.size(); ++i)
+  {
+    Bundle bundle;
+    getBundleConfiguration(configuration_file_list[i], bundle);
+
+    bundles.push_back(bundle);
+  }
+}
 
 void getMarkerFrame(int id, std::string& marker_frame)
 {
@@ -21,7 +115,7 @@ void getMarkerFrame(int id, std::string& marker_frame)
   marker_frame =string_stream.str();
 }
 
-bool toRosPose(Pose& alvar_pose, tf::Transform& ros_pose)
+bool toRosPose(const Pose& alvar_pose, tf::Transform& ros_pose)
 {
   for (size_t i = 0; i < 3; ++i)
   {
@@ -51,6 +145,17 @@ bool toRosPose(Pose& alvar_pose, tf::Transform& ros_pose)
   ros_pose = tf::Transform(rotation, origin);
 
   return true;
+}
+
+void toAlvarPose(const tf::Transform& ros_pose, Pose& alvar_pose)
+{
+  alvar_pose.translation[0] = ros_pose.getOrigin().x() * 100.0;
+  alvar_pose.translation[1] = ros_pose.getOrigin().y() * 100.0;
+  alvar_pose.translation[2] = ros_pose.getOrigin().z() * 100.0;
+  alvar_pose.quaternion[1] = ros_pose.getRotation().x();
+  alvar_pose.quaternion[2] = ros_pose.getRotation().y();
+  alvar_pose.quaternion[3] = ros_pose.getRotation().z();
+  alvar_pose.quaternion[0] = ros_pose.getRotation().w();
 }
 
 void makeMarkerMsg(int id, const std::string& reference_frame, const ros::Time& stamp, const tf::Transform& marker_wrt_output, int confidence, ar_track_alvar_msgs::AlvarMarker& marker_msg)
